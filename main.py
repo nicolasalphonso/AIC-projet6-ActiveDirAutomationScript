c6a48c701871856daed4b656baab927984c72785
Nicolas Alphonso <nicolas.alphonso@gmail.com>
2021-08-26 16:14:59 +0200

    tkinter try

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import os
 
 from dotenv import load_dotenv # use of python-dotenv to hide credentials and use environment variables
@@ -5,4 +8,20 @@ load_dotenv()  # take environment variables from .env
 
 from ldap3 import Server, Connection, SAFE_SYNC
 
-print(os.environ.get("ESSAI"))
+import menu # import of menu module
+
+# defining server and connexion variables from .env file informations
+serverAdress = os.environ.get("SERVER")
+user = os.environ.get("USER")
+password = os.environ.get("PASSWORD")
+
+# connection to LDAP server
+server = Server(host=serverAdress)
+# connection = Connection(server, user, password, client_strategy=SAFE_SYNC, auto_bind=True)
+connection = Connection(server, auto_bind=True)
+
+# verifiying connection for development purpose
+print(connection)
+
+# Main menu
+menu.display()
