28ed1bc3b2f064d75c06810c5151be9895305110
Nicolas Alphonso <nicolas.alphonso@gmail.com>
2021-08-26 17:25:11 +0200

    Tkinter test

--- menu.py
+++ menu.py
@@ -1,9 +1,18 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 
 def display():
     root = tk.Tk()
 
-    canvas = tk.Canvas
+    canvas = tk.Canvas(root, width=900, height=500)
+    canvas.grid(columnspan=6, rowspan=5)
+
+    # logo
+    logo = Image.open('logo.png')
+    logo = ImageTk.PhotoImage(logo)
+    logo_label = tk.Label(image=logo)
+    logo_label.image = logo
+    logo_label.grid(column=0, row=0)
 
     root.mainloop()
