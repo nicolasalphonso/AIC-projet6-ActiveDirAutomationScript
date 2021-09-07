3350b75961e48f80894094e6a83c27dfa5857988
Nicolas Alphonso <nicolas.alphonso@gmail.com>
2021-09-07 16:45:55 +0200

    unlock account function v1

--- unlockAccounts.py
+++ unlockAccounts.py
@@ -1,17 +1,23 @@
 from ldap3 import SUBTREE, MODIFY_REPLACE
 from ldap3.core.exceptions import LDAPException
 import os
-from datetime import datetime
+import time
 
 
 def unlock_accounts(connection):
-    initial_log = "Logs: Unlock accounts" + str(datetime.now())
+    # determine a good time based complement for the log filename
+    initial_time = time.strftime("%Y%m%d-%H%M%S")
+
+    # define the first line of the log file
+    initial_log = "Logs: Unlock accounts - " + initial_time
     logs = initial_log
 
+    # define search filter and search base
     search_filter = "(&(objectClass=user)(lockoutTime>=1))"
 
     search_base = os.environ.get("SEARCHBASE")
 
+    # search for all locked accounts
     try:
         connection.search(search_base=search_base,
                           search_filter=search_filter,
@@ -21,21 +27,34 @@ def unlock_accounts(connection):
     except LDAPException as e:
         logs += "\n Error : " + str(e)
 
+    # unlock each locked account
     for element in connection.response:
         try:
             user_dn = element["dn"]
-            print(user_dn)
+            print("locked user : " + user_dn)
 
-            # unlock user
+            # unlock user setting lockoutTime to 0
             try:
                 connection.modify(user_dn, {'lockoutTime': [(MODIFY_REPLACE, ['0'])]})
                 print("ok")
+
             except LDAPException as e:
                 logs += "\n Error : " + str(e)
-        except:
+
+        except Exception:
             pass
 
+    # if no log was written, just add "Nothing to declare" in the log file
     if logs == initial_log:
         logs += "\n Nothing to declare ! :)"
-
     print(logs)
+
+    # create the logs directory if it doesn't exist
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+
+    # create the log file in logs folder
+    log_file_name = "logs/unlock_accounts_" + initial_time + ".txt"
+    file = open(log_file_name, "a")
+    file.write(logs)
+    file.close()
