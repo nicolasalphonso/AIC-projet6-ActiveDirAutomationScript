8d73ca0088f3d12c23de3bc5a321cfb97d9e52ae
Nicolas Alphonso <nicolas.alphonso@gmail.com>
2021-09-07 17:46:02 +0200

    "add group" function v1

--- addGroup.py
+++ addGroup.py
@@ -1,14 +1,25 @@
-import tkinter as tk
 import os
-from ldap3.core.exceptions import LDAPException
+import tkinter as tk
+
 from dotenv import load_dotenv  # use of python-dotenv to hide credentials and use environment variables
+from ldap3.core.exceptions import LDAPException
+
 load_dotenv()
+
+'''
+This function displays the form and calls the function to create a group
+'''
 
 
 def display(connection):
+    # generate the window
     root = tk.Tk()
     root.geometry("900x500")
     root.title("Add a group")
+
+    '''
+    This function handles the creation of a group
+    '''
 
     def add_a_group():
         # delete content of displayLabel
@@ -18,8 +29,10 @@ def display(connection):
         cn = input_cn.get()
         description = input_description.get()
 
+        # if fields are not empty
         if (cn != "") and (description != ""):
 
+            # generate the dn and the attribute of the new group
             dl_shortname = cn
             dl_group_dn = 'CN=' + dl_shortname + os.environ.get("SEARCHDC")
             object_class = 'group'
@@ -30,15 +43,18 @@ def display(connection):
                 'sAMAccountName': dl_shortname
             }
 
+            # add the group
             try:
                 connection.add(dl_group_dn, object_class, attr)
 
             except LDAPException as e:
                 connection.result['description'] = e
 
+        # else display that all fields are required
         else:
             connection.result['description'] = "All fields required"
 
+        # display the description of the result of the operation
         result_display.config(text=connection.result['description'])
         print(connection.result['description'])
 
@@ -74,4 +90,5 @@ def display(connection):
     result_display = tk.Label(root, text="", font=("Raleway", 22))
     result_display.pack()
 
+    # tkinter main loop
     root.mainloop()
